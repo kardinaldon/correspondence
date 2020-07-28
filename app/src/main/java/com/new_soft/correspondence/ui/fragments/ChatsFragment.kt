f8116b42ad931cb875cc69d4680c0aefe730a281
Andrew <kardinalmail@gmail.com>
2020-07-28 08:49:45 +0300

    refactoring

---
+++ app/src/main/java/com/new_soft/correspondence/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,10 @@
+package com.new_soft.correspondence.ui.fragments
+
+import com.new_soft.correspondence.R
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ app/src/main/java/com/new_soft/correspondence/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.new_soft.correspondence.ui.fragments
+
+import com.new_soft.correspondence.R
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
